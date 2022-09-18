6b7e1c704b9583e13d530cf8205d40d29d610933
Petteri Ponkamo <petteri.ponkamo@gmail.com>
2022-09-18 10:51:20 +0300

    Read from config directly and change config format

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use std::io::Read;
 use std::io::{self, Write};
 use std::process::Command;
 
+static SEPARATOR: &str = " ";
+static ACCOUNT_ROW_MARK: &str = "__account";
+
+fn exit_with_message(message: &str) {
+    println!("{}", message);
+    std::process::exit(0x0010);
+}
+
 fn read_config_file() -> Result<String, io::Error> {
     let mut f = File::open("config.txt")?;
     let mut s = String::new();
@@ -11,16 +19,24 @@ fn read_config_file() -> Result<String, io::Error> {
 }
 
 fn get_url(account: String, repo: String) -> String {
-    let separator: &str = " ";
     let mut url: String = String::from("");
+    if account == ACCOUNT_ROW_MARK {
+        exit_with_message("Illegal account name '__account'");
+    }
     match read_config_file() {
         Ok(config_file) => {
             let rows: Vec<&str> = config_file.split("\n").collect();
-            for config in rows.iter() {
-                let config_data: Vec<&str> = config.split(separator).collect();
-                if account == config_data[0] && repo == config_data[1] {
-                    url.push_str(config_data[2]);
-                    url.push_str(config_data[3]);
+            for row in rows.iter() {
+                let row_data: Vec<&str> = row.split(SEPARATOR).collect();
+                if row_data[0] == ACCOUNT_ROW_MARK {
+                    url.push_str(row_data[2]);
+                }
+            }
+
+            for row in rows.iter() {
+                let row_data: Vec<&str> = row.split(SEPARATOR).collect();
+                if row_data[0] == account && row_data[1] == repo {
+                    url.push_str(row_data[2]);
                     return url;
                 }
             }
@@ -44,7 +60,7 @@ fn add_to_config(row: String) -> Result<(), io::Error> {
 fn on_add_repo() {
     let repo_name: String = std::env::args().nth(2).expect("no repo name given");
     let repo_target: String = std::env::args().nth(3).expect("no repo target given");
-    let row = format!("\n{}:{}", repo_name, repo_target);
+    let row = format!("\n{} {}", repo_name, repo_target);
     add_to_config(row).expect("Could not write");
 }
 
@@ -53,13 +69,14 @@ fn main() {
 
     if root_or_command == "add-repo".to_string() {
         on_add_repo();
-        std::process::exit(0x0010);
+        exit_with_message("Repository added!");
     }
 
     let repo = std::env::args().nth(2).expect("no repo given");
 
     let url = get_url(root_or_command, repo);
     println!("url {:?}", url);
+
     let output = Command::new("open")
         .args(&[&url])
         .output()
